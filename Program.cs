a4e58c8a3da7d1b8a863001e662617a3cb6976a6
mzaizai80 <89834896+mzaizai80@users.noreply.github.com>
2022-10-07 11:22:33 +0200

    Working FIZZ BUZZ with Switch Statement

--- Program.cs
+++ Program.cs
@@ -1,4 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-Console.WriteLine("Hello, World!");
+
+for (int i = 0; i < 30; i++)
+{
+    Console.WriteLine(GetoutputString(i));
+}
+
+static string GetoutputString(int i)
+{
+    return (i % 3, i % 5) switch
+    {
+        (0,0)=>"FizzBuzz",
+        (_,0)=>"Bizz",
+        (0,_)=>"Fizz",
+        (_,_)=> i.ToString()
+    };
+}
+
+
